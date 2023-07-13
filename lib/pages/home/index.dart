f566cb7f12e8748126a43491d713a10ed073d8d4
李锐 <ohohua@163.com>
2023-07-13 23:28:47 +0800

    route have login style

--- lib/pages/home/index.dart
+++ lib/pages/home/index.dart
@@ -3,12 +3,50 @@ import 'package:flutter/material.dart';
 // 引入自定义组件
 import 'package:rent/widgets/page_content.dart';
 
+// 1.需要准备 4 个 tab内容区（tabView）
+List<Widget> tabViewList = [
+  const PageContent(name: '首页'),
+  const PageContent(name: '搜索'),
+  const PageContent(name: '咨询'),
+  const PageContent(name: '我的'),
+];
+
+// 2.需要准备 4 个 BottomNavigationBarItem
+List<BottomNavigationBarItem> barItemList = [
+  const BottomNavigationBarItem(icon: Icon(Icons.home), label: '首页'),
+  const BottomNavigationBarItem(icon: Icon(Icons.search), label: '搜索'),
+  const BottomNavigationBarItem(icon: Icon(Icons.info), label: '咨询'),
+  const BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: '我的'),
+];
+
 // 编写无状态组件
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const PageContent(name: '首页');
+    return Scaffold(
+      body: tabViewList[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: barItemList,
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.green,
+        onTap: onItemTapped,
+      ),
+    );
   }
 }
